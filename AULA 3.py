a88f05b6f0d02c04a51558f44ebb7cd9bad4c3e1
ArthurFrizzo <161754046+ArthurFrizzoDrey@users.noreply.github.com>
2024-02-29 15:42:10 -0300

    Add files via upload

---
+++ AULA 3.py
@@ -0,0 +1,3 @@
+nota = 10
+print(nota)
+print('O garoto mateus recebeu a nota',nota+10) 
---
+++ AULA 4.py
@@ -0,0 +1,11 @@
+print('1- Escolha o café de sua preferência')
+print('2- Esquente a água na temperatura de sua preferência')
+print('3- Coloque o filtro de café na tampa da térmica')
+print('4- Coloque o quanto de café você irá consumir no filtro')
+print('5- Depois de esquentada, coloque a água aos pouco no filtro')
+print('6- Espere escoar todo o café')
+print('7- Pegue uma xícara de seu gosto')
+print('8- Sirva o café e aproveite')
+
+
+
