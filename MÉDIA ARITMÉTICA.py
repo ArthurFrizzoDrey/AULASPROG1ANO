a1dff611ca075c2df9a1b6af884f8166f7225b19
Arthur Frizzo <161754046+ArthurFrizzoDrey@users.noreply.github.com>
2024-03-14 16:36:53 -0300

    Add files via upload

---
+++ MÉDIA ARITMÉTICA.py
@@ -0,0 +1,14 @@
+N1= 5
+N2= 7
+N3= 5
+N4= 3
+
+M=(N1+N2+N3+N4)/4
+if(M>=6):
+   print('Aprovado')
+
+elif (M<5):
+           print('Reprovado')
+else:
+     print('Recuperação')
+print ('O aluno tirou a nota',M) 
