59321a281712ddef69d26403d6d7430fc65ae875
Arthur Frizzo <161754046+ArthurFrizzoDrey@users.noreply.github.com>
2024-03-21 16:21:23 -0300

    Add files via upload

---
+++ AULAS VÁRIAVEIS.py
@@ -0,0 +1,39 @@
+# Váriaveis
+# Definição das primeira variáveis e constantes
+# Trabalhando com operadores
+a = 3
+b = 7
+c= (a+b)-(b+a)
+print(c)
+print((a+b)-(b+a))
+print((a-b)*(a+c))
+print((a-c)*(a+b))
+print(a==b or b==c)
+
+# Mudança de tipo de uma Variável
+a = 'Agora eu entendi tudo,'
+b = 'Graças ao meu querido professor'
+print(a+ b)
+print(b)
+#Definindo uma variável com base no valor atual de outra
+c = a+b
+print(c)
+#Definição de multiplas variáveis com valores iguais
+x=y=z=10
+print(x)
+print(y)
+print(z)
+# Definição de múltiplas variáveis com valores diferentes
+x,y,z=10,20,30
+print(x)
+print(y)
+print(z)
+# Captação de input do usuário
+# string: tipo de dado (texto/literal)
+nome= str(input('Olá, qual o seu nome?'))
+print('Olá,', nome)
+# int: tipo de dado (Número inteiro)
+número=int(input('Digite um número'))
+# bool: tipo de dado booleano (true or false)
+
+
