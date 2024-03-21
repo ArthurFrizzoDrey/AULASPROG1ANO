e973c408a5664f3e9c93fc2aeb51bfa7bebbb27b
Arthur Frizzo <161754046+ArthurFrizzoDrey@users.noreply.github.com>
2024-03-21 16:35:07 -0300

    Add files via upload

---
+++ Atividade.py
@@ -0,0 +1,8 @@
+número=int(input('Digite um valor'))
+if número > 10 :
+    print('O número é maior que 10')
+elif número==10 :
+    print('O número é igual a 10')
+else :
+    print('O número é menor que 10')
+# obs: na linha 2 poderia ser if número >=10 :)
