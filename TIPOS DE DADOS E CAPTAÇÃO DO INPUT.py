4d6b1dfe80c498259c2dab4ec2e6621e002622cf
Arthur Frizzo <161754046+ArthurFrizzoDrey@users.noreply.github.com>
2024-03-21 16:36:34 -0300

    Add files via upload

---
+++ TIPOS DE DADOS E CAPTAÇÃO DO INPUT.py
@@ -0,0 +1,7 @@
+# Captação de input do usuário
+# string: tipo de dado (texto/literal)
+nome= str(input('Olá, qual o seu nome?'))
+print('Olá,', nome)
+# int: tipo de dado (Número inteiro)
+número=int(input('Digite um número'))
+# bool: tipo de dado booleano (true or false)
