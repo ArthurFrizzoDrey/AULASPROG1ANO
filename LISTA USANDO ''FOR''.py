236484a08b3a1b4a3793aec04d534b5dcc631b30
Arthur Frizzo <161754046+ArthurFrizzoDrey@users.noreply.github.com>
2024-04-05 10:25:03 -0300

    Add files via upload

---
+++ LISTA USANDO ''FOR''.py
@@ -0,0 +1,9 @@
+#for: automatiza tarefas que exigem repetição
+
+lista_produtos= ["iphone","ipad","notebook","applewatch"]
+for produto in lista_produtos:
+    print(produto)
+
+lista_preços= [1500,2000,3000,4000]
+for preço in lista_preços:
+    print(preço + 50)
