1981beaae95e09ca532cfc81cb9e2b24d3f4baa9
Arthur Frizzo <161754046+ArthurFrizzoDrey@users.noreply.github.com>
2024-04-07 19:45:38 -0300

    Add files via upload

---
+++ USO DO INDEX E COUNT.py
@@ -0,0 +1,9 @@
+#USO DO INDEX
+lista=['banana','maçã','uva','morango']
+índice=lista.index('uva')
+print('O índice do item uva na lista é :',índice)
+
+#USO DO COUNT
+lista=['banana','maçã','uva','morango','morango']
+quantidade=lista.count('morango')
+print('A quantidade de morangos na lista é de:',quantidade)
