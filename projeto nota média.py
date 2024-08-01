f65a7df6a7ac57c32b18b16a6df15c2da20ad0a8
Arthur Frizzo <161754046+ArthurFrizzoDrey@users.noreply.github.com>
2024-08-01 15:05:21 -0300

    Add files via upload

---
+++ projeto nota média.py
@@ -0,0 +1,12 @@
+#faça um programa que calcule sua aprovação com seguintes configurações:
+A1= 4
+A2= 3
+A3= 4
+A4= 10
+media= (A1+A2+A3+A4)/4
+if media >7:
+    print('Aprovado,sua média é de',media)
+elif media >5:
+    print('Recuperação,sua média é de',media)
+elif media < 5:
+    print('Reprovado,sua média é de',media)
