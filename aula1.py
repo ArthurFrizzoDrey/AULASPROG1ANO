e8a6257104c6756d2a3e240c4dcf597861a0b2be
Arthur Frizzo <161754046+ArthurFrizzoDrey@users.noreply.github.com>
2024-08-01 15:07:46 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,29 @@
+# Váriaveis
+# Definição das primeira variáveis e constantes
+# Trabalhando com operadores
+a = 3
+b = 7
+c= (a+b)-(b+a)
+print(c)
+print((a+b)-(b+a))
+print((a-b)*(a+c))
+print((a-c)*(a+b))
+print(a==b or b==c)
+
+print('')
+print('')
+
+# Mudança de tipo de uma Variável
+a = 'Agora eu entendi tudo,'
+b = 'Graças ao meu querido professor'
+print(a+ b)
+print(b)
+#Definindo uma variável com base no valor atual de outra
+c = a+b
+print(c)
+
+#captação de input do usuario
+nome= str(input('Qual é o seu nome?'))
+print('Olá,',nome)
+
+
