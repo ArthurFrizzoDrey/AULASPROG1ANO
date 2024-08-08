6fa7adbe4f55a8f13fa1c4b8bc6dfde15e44a37a
Arthur Frizzo <161754046+ArthurFrizzoDrey@users.noreply.github.com>
2024-08-08 16:34:21 -0300

    Add files via upload

---
+++ exerciciosLISTASAVANÇADO.py
@@ -0,0 +1,34 @@
+#Crie uma lista com os nomes de 5 animais. Substitua o nome do primeiro animal da lista por um novo nome. Imprima a lista modificada.
+lista_animais= ['Cachorro','Gato','Girafa','Hipopotamo','leao']
+print(lista_animais)
+lista_animais.pop(0)
+lista_animais.insert(0,'coala')
+print(lista_animais)
+print()
+
+
+#Crie uma lista com números repetidos. Remova as duplicatas da lista e imprima a lista sem duplicatas
+lista_repetidos= [40,40,50,20,10,10]
+print(lista_repetidos)
+lista_repetidos.pop(0)
+lista_repetidos.pop(4)
+print(lista_repetidos)
+print()
+
+
+#Crie uma lista com frases. Converta cada frase para letras maiúsculas e imprima as frases modificadas.
+lista_frases= ['olá,tenho um ótimo dia!', 'have a nice day!']
+print(lista_frases)
+print('[OLÁ, TENHA UM ÓTIMO DIA!, HAVE A NICE DAY!]')
+
+
+
+
+
+
+#Crie uma matriz 2x2 (uma lista de listas). Acesse e imprima cada elemento da matriz
+
+
+
+
+#Crie uma lista com os quadrados dos números de 1 a 10 usando list comprehension.
