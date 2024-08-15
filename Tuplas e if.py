4c705cccb38450b76b40b43e8f197bdf4b2d2457
Arthur Frizzo <161754046+ArthurFrizzoDrey@users.noreply.github.com>
2024-08-15 16:24:15 -0300

    Add files via upload

---
+++ Tuplas e if.py
@@ -0,0 +1,9 @@
+tupla = (1,2,3)
+lista= [4,5,6]
+dicionario= {'nome':'Maria','idade': 25}
+if 2 in tupla:
+    print('O número 2 está na tupla')
+if len(lista) ==3:
+    print('A lista tem 3 elementos')
+if 'nome' in dicionario:
+    print('A chave nome existe no dicionario') 
