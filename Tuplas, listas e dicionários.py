c18e7b1280738196b25b93bf59fbd95ee4ba7129
Arthur Frizzo <161754046+ArthurFrizzoDrey@users.noreply.github.com>
2024-08-15 15:15:12 -0300

    Add files via upload

---
+++ Tuplas, listas e dicionários.py
@@ -0,0 +1,8 @@
+#Tupla (IMUTÁVEL, ORDENADA)
+frutas = ('maça','banana','uva')
+
+#Lista (MUTÁVEL, ORDENADA)
+compras= ['leite','ovos','pao']
+
+#Dicionário (MUTÁVEL, DESORDENADA)
+pessoa = {'nome','RG','data de aniversario'}
