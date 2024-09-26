5825cc93140ae2811307f9e3bbf45e9deac91a9f
Arthur Frizzo <161754046+ArthurFrizzoDrey@users.noreply.github.com>
2024-09-26 15:03:30 -0300

    Add files via upload

---
+++ Exercícios while com if.py
@@ -0,0 +1,28 @@
+#atividade contador de 0 a 10
+import time
+
+contador= 0
+while contador <11:
+    print(contador)
+    contador+=1
+
+# Escreva um programa que peça um número ao usuario e continue pedindo até que o número seja maior que 10
+número= int(input('Escreva um número'))
+while número <=10:
+    print('NÚMERO ERRADO! ESCREVA OUTRO!')
+    número = int(input('Escreva um número'))
+else:
+    print('NÚMERO ACEITADO!')
+
+
+
+#Utilize um while para calcular a soma dos números de 1 a 100
+soma= 0
+n= 1
+while n <101:
+    print(n)
+    n+=n
+
+
+
+
